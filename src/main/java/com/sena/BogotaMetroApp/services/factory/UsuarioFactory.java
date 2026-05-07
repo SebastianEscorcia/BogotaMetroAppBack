9f47c780fe93de970aa214c30ee4e5a3f9856de2
SebastianEscorcia <sebastianescorcia18@gmail.com>
2026-05-06 22:54:12 -0500

    feat: Add method encoder in the factory service for password

--- src/main/java/com/sena/BogotaMetroApp/services/factory/UsuarioFactory.java
+++ src/main/java/com/sena/BogotaMetroApp/services/factory/UsuarioFactory.java
@@ -25,7 +25,7 @@ public class UsuarioFactory {
 
         Usuario usuario = new Usuario();
         usuario.setCorreo(dto.getCorreo());
-        usuario.setClave(dto.getClave());
+        usuario.setClave(encoder.encode(dto.getClave()));
         usuario.setActivo(true);
         usuario.setRol(rol);
 
